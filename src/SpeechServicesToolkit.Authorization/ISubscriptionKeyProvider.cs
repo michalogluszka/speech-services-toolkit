dbd0d39e56eccd873e5d40afc7ba5d246997d2e5
Michal Ogluszka <michal.ogluszka@outlook.com>
2018-07-19 22:52:36 +0200

    moved authorization

---
+++ src/SpeechServicesToolkit.Authorization/ISubscriptionKeyProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeechServicesToolkit.Authorization
+{
+    public interface ISubscriptionKeyProvider
+    {
+        /// <summary>
+        /// Provides endpoint and subscription key
+        /// </summary>
+        /// <returns>endpoint,subscriptionkey</returns>
+        string GetSubscriptionKey();
+    }
+}
