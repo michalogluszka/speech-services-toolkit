284894d405b9e834280d59d8ad26e88bbec0f322
Michal Ogluszka <michal.ogluszka@outlook.com>
2018-10-11 22:31:50 +0200

    recorder introduced

---
+++ src/SpeechServicesToolkit/STT/Recorder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeechServicesToolkit.STT
+{
+    public class Recorder
+    {
+        private string _requestUri = "https://westeurope.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?language=en-US";
+    }
+}
